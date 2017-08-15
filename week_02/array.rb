32fae7b919e5c16c79c7e168ca2f0c0cbcb8b466
Kyle Miller <kylemiller995@gmail.com>
2017-08-15 12:56:39 +0100

    add array

---
+++ week_02/array.rb
@@ -0,0 +1,9 @@
+
+def add_to_array(*favourite_movie)
+  array_of_favourite_movies = []
+  array_of_favourite_movies << favourite_movie
+  puts array_of_favourite_movies
+end
+
+
+add_to_array("the lion the wizard and the cupboard","movie2","movie3")
---
+++ week_02/hash.rb
@@ -0,0 +1,21 @@
+favourite_movie = [
+  {
+    :director "pedro jackson"
+    :run_time_minutes 215
+    :title "lotr"
+  },
+
+  {
+    :director ""
+    :run_time_minutes 215
+    :title ""
+  },
+  {
+    :director "pedro jackson"
+    :run_time_minutes 215
+    :title "lotr"
+  },
+]
+
+def return_director(position)
+  puts position[]
